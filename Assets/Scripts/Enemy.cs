d90e021636e4f76c7912085abe2c32becfa02780
luthfan360 <luthfan360@gmail.com>
2021-11-29 20:39:00 +0700

    Added Enemy Explosion Animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,11 +7,15 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     int speed = 5;
     Player player;
+    Animator anim;
+    BoxCollider2D collider;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
+        anim = GetComponent<Animator>();
+        collider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -29,15 +33,19 @@ public class Enemy : MonoBehaviour
     {
         if (other.tag == "Laser")
         {
-            Destroy(this.gameObject);
+            Destroy(this.gameObject, 2.7f);
             Destroy(other.gameObject);
             player.addScore();
+            anim.SetTrigger("EnemyHit");
+            collider.enabled = false;
         }
         if (other.tag == "Player")
         {
-            Destroy(this.gameObject);
+            Destroy(this.gameObject, 2.7f);
             player.Damaged();
             player.addScore();
+            anim.SetTrigger("EnemyHit");
+            collider.enabled = false;
         }
     }
 }
